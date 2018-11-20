da87fafd6be84a5d5bd3940afd366c47e7f75e25
Daniels <daniel.sadowski75@gmail.com>
2018-11-20 23:07:08 +0100

    fist app

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {Article} from './model/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log('Title: ' + title.value + ' link: ' + link.value);
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a, b) =>  b.votes - a.votes);
+  }
+
+}
